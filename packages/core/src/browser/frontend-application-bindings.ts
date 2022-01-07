03c227b98868c7b89dee4f9f6c180ecfae14dcba
Colin Grant <colin.grant@ericsson.com>
2022-01-06 17:27:40 -0700

    Add validation service for editor preferences

--- packages/core/src/browser/frontend-application-bindings.ts
+++ packages/core/src/browser/frontend-application-bindings.ts
@@ -19,7 +19,7 @@ import { bindContributionProvider, DefaultResourceProvider, MessageClient, Messa
 import {
     bindPreferenceSchemaProvider, PreferenceProvider,
     PreferenceProviderProvider, PreferenceSchemaProvider, PreferenceScope,
-    PreferenceService, PreferenceServiceImpl
+    PreferenceService, PreferenceServiceImpl, PreferenceValidationService
 } from './preferences';
 
 export function bindMessageService(bind: interfaces.Bind): interfaces.BindingWhenOnSyntax<MessageService> {
@@ -41,6 +41,7 @@ export function bindPreferenceService(bind: interfaces.Bind): void {
     bind(PreferenceServiceImpl).toSelf().inSingletonScope();
     bind(PreferenceService).toService(PreferenceServiceImpl);
     bindPreferenceSchemaProvider(bind);
+    bind(PreferenceValidationService).toSelf().inSingletonScope();
 }
 
 export function bindResourceProvider(bind: interfaces.Bind): void {
